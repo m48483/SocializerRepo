23a33cb169b51d3f4c4501182ebb07b6348f6f70
m48483 <71882579+m48483@users.noreply.github.com>
2021-04-15 22:49:31 +0900

    [#01] 대화 시스템 재구현
    lobby에 새로 구현한 대화 시스템 적용
    
    //모든 scene에 적용 시킨 후 올리려 했는데 바쁘다 보니 그러지 못했습니다... :( 더 일찍 올렸어야 했는데 죄송합니다.

---
+++ New Unity Project/Assets/NewBehaviourScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        float a;    // 변수 선언
+        float b;
+
+        a = 3.14f;  // 저장
+        b = 4.05f;
+
+        float sum, sub, mul, div;   //더하기 빼기 곱하기 나누기
+        sum = a + b;
+        sub = a - b;
+        mul = a * b;
+        div = a / b;
+
+        Debug.Log(sum);
+        Debug.Log(sub);
+        Debug.Log(mul);
+        Debug.Log(div);
+    }
+}
