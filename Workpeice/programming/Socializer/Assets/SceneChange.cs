1f5042398326d59b17a1f46fce09f01ca3bb38f1
Lesin928 <seung1084@gmail.com>
2021-07-20 21:42:33 +0900

    [#0] 이름 입력 씬 구현
    1. 타이틀 씬으로 전환 구현
    2. 프롤로그 씬으로 전환 구현
    3. 입력 필드 및 텍스트 구현
    4. PlayerPrefs를 이용한 이름 저장 구현
    5. 문자 검사 및 입력 검사 구현

---
+++ Workpeice/programming/Socializer/Assets/SceneChange.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChange : MonoBehaviour
+{
+    public void TitleChange()
+    {
+        SceneManager.LoadScene("Title");
+    }
+    public void PrologueChange()
+    {
+        SceneManager.LoadScene("Prologue");
+    }
+}
