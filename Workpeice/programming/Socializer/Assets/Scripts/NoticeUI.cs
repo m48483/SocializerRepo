50103084b936a66c2059605b15db5869c5576128
Lesin928 <seung1084@gmail.com>
2021-07-31 07:51:39 +0900

    선택지 있는 팝업창 참고용 구현
    프로롤로그 씬에 참고용으로
    선택지 있는 팝업창 구현해둠.
    버튼 누르면 실행되고 yes나 no 누르면 닫힘 (변수전달은 미구현해둠)
    
    씬 확인하다가 메타파일 수정될수 있으니 해당 씬 수정사항은 그냥 discard 할것

--- Workpeice/programming/Socializer/Assets/Scripts/NoticeUI.cs
+++ Workpeice/programming/Socializer/Assets/Scripts/NoticeUI.cs
@@ -9,6 +9,9 @@ public class NoticeUI : MonoBehaviour
     public GameObject subbox;
     public Text subintext;
     public Animator subani;
+    public Button YButton;
+    public Button NButton;
+
     //코루틴 딜레이 2개 미리 선언
     private WaitForSeconds _UIDelay1 = new WaitForSeconds(2.0f);
     private WaitForSeconds _UIDelay2 = new WaitForSeconds(0.3f);
@@ -18,8 +21,10 @@ public class NoticeUI : MonoBehaviour
         subbox.SetActive(false); //알림창은 미리 비활성화
     }
     // 서브 메세지 >> string값을 매개변수로 받아와서 2초간 출력
-    // 사용법 : _notice.SUB("문자열");
-    public void SUB(string message)
+    // 사용법 : _notice.SUB("문자열"); notice는 참조된 변수 이름
+   
+    //선택지 없는 팝업
+    public void SUB(string message) //
     {
         subintext.text = message;
         subbox.SetActive(false);
@@ -27,7 +32,7 @@ public class NoticeUI : MonoBehaviour
         StartCoroutine(SUBDelay());
     }
 
-    IEnumerator SUBDelay()
+    IEnumerator SUBDelay() 
     {
         subbox.SetActive(true);
         subani.SetBool("isOn", true);
@@ -37,4 +42,46 @@ public class NoticeUI : MonoBehaviour
         subbox.SetActive(false);
 
     }
+
+    //선택지 있는 팝업
+    public void SelectSUB(string message) 
+    {
+        subintext.text = message;
+        subbox.SetActive(false);
+        StopAllCoroutines();
+        StartCoroutine(SelectDelay());
+    }
+
+    IEnumerator SelectDelay() //선택지 없은 경우
+    {
+        subbox.SetActive(true);
+        subani.SetBool("isOn", true);        
+        yield return _UIDelay1;
+    }
+
+    public void YesButton()
+    {
+        subani.SetBool("YesNo", true);
+        subani.SetTrigger("Select");
+        StartCoroutine(SelectOut());
+    }
+
+    public void NoButton()
+    {
+        subani.SetBool("YesNo", false);
+        subani.SetTrigger("Select");
+        StartCoroutine(SelectOut());
+    }
+
+    IEnumerator SelectOut() //선택지 없은 경우
+    {
+        subani.SetBool("isOn", false);
+        yield return _UIDelay2;
+        subbox.SetActive(false);
+    }
+
+
+
+
+
 }
---
+++ Workpeice/programming/Socializer/Assets/Scripts/test.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class test : MonoBehaviour
+{
+    NoticeUI _notice;
+
+    private void Awake()
+    {
+        _notice = FindObjectOfType<NoticeUI>();
+    }
+
+    public void Test()
+    {
+        _notice.SelectSUB("하나만 선택해");
+    }
+
+}
