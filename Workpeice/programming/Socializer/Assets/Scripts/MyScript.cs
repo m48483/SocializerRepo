30ee2392e4c10808a1bd2c13225d2ec612faa667
m48483 <71882579+m48483@users.noreply.github.com>
2021-08-24 03:19:53 +0900

    팝업창

---
+++ Workpeice/programming/Socializer/Assets/Scripts/MyScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MyScript : MonoBehaviour
+{
+    public void OnClickButton()
+    {
+        PopupSystem.instance.OpenPopup(
+            () =>
+            {
+                Debug.Log("OnClick Yes");
+            },
+            () =>
+            {
+                Debug.Log("Onclick No");
+            });
+    }
+}
