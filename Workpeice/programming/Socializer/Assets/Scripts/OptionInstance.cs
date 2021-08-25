45a52780ad7947428b353c62e5e8b4ebcfe71171
m48483 <71882579+m48483@users.noreply.github.com>
2021-08-25 20:45:21 +0900

    설정창 비디오 해상도
    1. 설정창 비디오 해상도 설정
    - 현재 쓸모 없는 해상도도 포함된 상태. 추후 필요한 것들로만 구현할 예정
    - 창모드 설정 가능

---
+++ Workpeice/programming/Socializer/Assets/Scripts/OptionInstance.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OptionInstance : MonoBehaviour
+{
+    public void OnClickButton()
+    {
+        OptionPopupSystem.instance.OpenPopup(
+            () =>
+            {
+                Debug.Log("OnClick Btn");
+            });
+    }
+}
