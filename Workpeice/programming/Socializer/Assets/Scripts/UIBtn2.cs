3519db53ab9caa43e81c9ab6e6c84af6bdcb2443
Horim1 <72972218+horimihorimi@users.noreply.github.com>
2021-09-04 15:55:54 +0900

    UI씬 업로드

---
+++ Workpeice/programming/Socializer/Assets/Scripts/UIBtn2.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+
+public class testprint1 : MonoBehaviour
+{
+    public GameObject ButtonName;
+    public int Testnumber = 1;
+
+    public void Awake()
+    {
+        ButtonName = GameObject.Find("testButton");
+    }
+
+    public void Clicked()
+    {
+        Debug.Log(ButtonName.name + " 버튼 클릭");
+        Debug.Log("메시지 전달 확인");
+    }
+}
