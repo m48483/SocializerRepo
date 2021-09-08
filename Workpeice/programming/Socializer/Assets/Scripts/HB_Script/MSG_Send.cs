5528f69c46eab719ec23e6da418c9952923f7e80
Horim1 <72972218+horimihorimi@users.noreply.github.com>
2021-09-08 22:14:06 +0900

    UI씬 업로드
    Schedule_Button 스크립트 제작

---
+++ Workpeice/programming/Socializer/Assets/Scripts/HB_Script/MSG_Send.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MSG_Send : MonoBehaviour
+{
+
+    void Start()
+    {
+        GameObject.Find("Canvas").transform.FindChild("DelayButton").gameObject.SetActive(true);
+        Debug.Log("실행했음");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Workpeice/programming/Socializer/Assets/Scripts/HB_Script/Schedule_Button.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Schedule_Button : MonoBehaviour
+{
+    public GameObject Button;
+    [SerializeField] Texture2D cursorImg;
+    public void Clicked()
+    {
+        Button = EventSystem.current.currentSelectedGameObject;
+
+        if(Button.name == "Observation")
+        {
+            Cursor.SetCursor(cursorImg, Vector2.zero, CursorMode.ForceSoftware);
+            Debug.Log("관찰 버튼 클릭");
+            //sendmsg
+        }
+        
+        else if(Button.name == "Action")
+        {
+            GameObject.Find("Canvas").transform.FindChild("Pass").gameObject.SetActive(true);
+            GameObject.Find("Canvas").transform.FindChild("Arrest").gameObject.SetActive(true);
+            GameObject.Find("Canvas").transform.FindChild("Kill").gameObject.SetActive(true);
+            Debug.Log("행동 버튼 클릭");
+
+        }
+    }
+}
