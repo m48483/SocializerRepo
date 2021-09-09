861f3f1e3663617ea0da3e051e32e11879b1a7d0
143xx <qnwk963@naver.com>
2021-09-10 03:56:34 +0900

    볼륨 에러 수정
    - bgm 중복 문제를 작일 새벽 해결했으나 볼륨 슬라이더가 먹통이 되는 문제가 생김 (DontDestroyOnLoad와 Destroy 하면서 다른 캔버스에 있는 슬라이더 오브젝트의 연결이 끊김)
    - 이를 OpenSlider 스크립트를 생성하여 OptionPopupSystem 스크립트에 static bool 변수를 이용하여 해결함
    - 다른 scene 이동 후 다시 돌아와도 bgm 중복, 슬라이더 에러 없음
    
    * 현재 master volume slider만 구현된 상태
    - 추후 scene이 늘어나도 bgm 및 효과음 사용이 늘어나면 나머지 슬라이더도 구현할 예정
    * OptionCanvas 프리팹이 수정되었습니다. (Sliders 삭제, Sliders: AudioManager의 자식 오브젝트로 변경, DontDestroyOnLoad로 사용됨)
    * 익일, 비디오 해상도 문제도 해결해서 올릴 예정 (프리팹 변경)

--- Workpeice/programming/Socializer/Assets/Scripts/AudioManager.cs
+++ Workpeice/programming/Socializer/Assets/Scripts/AudioManager.cs
@@ -6,10 +6,17 @@ using UnityEngine.SceneManagement;
 public class AudioManager : MonoBehaviour
 {
     public static AudioManager instance = null;
+    OpenSliders openSliders;
     void Awake()
     {
+        openSliders = GetComponent<OpenSliders>();
+
         if (instance != null)
+        {
             Destroy(this.gameObject);
+            Debug.Log(OpenSliders.isSliderOn);
+            OptionPopupSystem.isOn = false;
+        }
 
         instance = this;
         DontDestroyOnLoad(this);
--- Workpeice/programming/Socializer/Assets/Scripts/OptionPopupSystem.cs
+++ Workpeice/programming/Socializer/Assets/Scripts/OptionPopupSystem.cs
@@ -7,6 +7,7 @@ using UnityEngine.UI;
 public class OptionPopupSystem : MonoBehaviour
 {
     public GameObject popup;
+    public static bool isOn = false;
 
     public static OptionPopupSystem instance { get; private set; }
 
@@ -14,6 +15,7 @@ public class OptionPopupSystem : MonoBehaviour
 
     private void Awake()
     {
+        isOn = this;
         instance = this;
     }
 
@@ -21,6 +23,8 @@ public class OptionPopupSystem : MonoBehaviour
     {
         this.onClickBtn = onClickBtn;
         popup.SetActive(true);
+        isOn = true;
+        Debug.Log("PopupOn");
     }
 
     public void OnClickBtn()
@@ -31,5 +35,7 @@ public class OptionPopupSystem : MonoBehaviour
     void ClosePopUP()
     {
         popup.SetActive(false);
+        isOn = false;
+        Debug.Log("PopupOff");
     }
 }
