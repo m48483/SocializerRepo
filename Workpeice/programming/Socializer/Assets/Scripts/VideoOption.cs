83cb938fbf85ad75f53bf02c140eff8e8ea8c839
143xx <qnwk963@naver.com>
2021-09-11 13:29:53 +0900

    비디오 해상도 에러 수정
    - 60hz가 아닌 경우 생기는 드롭다운 에러 수정함
    - 1920x1080, 1600x900, 1280x720 만 가능
    - 프리팹 수정되었습니다

--- Workpeice/programming/Socializer/Assets/Scripts/VideoOption.cs
+++ Workpeice/programming/Socializer/Assets/Scripts/VideoOption.cs
@@ -5,55 +5,30 @@ using UnityEngine.UI;
 
 public class VideoOption : MonoBehaviour
 {
-    public Dropdown resolutionDropdown;
-    List<Resolution> resolutions = new List<Resolution>();
-    public int resolutionNum;
+    public Dropdown dropdown;
+    public int dropdownNum;
     FullScreenMode screenMode;
     public Toggle windowedscreenBtn;
 
-    void Start()
-    {
-        InitUI();
-    }
-
-    void InitUI()
-    {
-        for(int i = 0; i < Screen.resolutions.Length; i++)
-        {
-            if (Screen.resolutions[i].refreshRate == 60)
-                resolutions.Add(Screen.resolutions[i]);
-        }
-        resolutionDropdown.options.Clear();
-
-        int optionNum = 0;
-
-        foreach(Resolution item in resolutions)
-        {
-            Dropdown.OptionData option = new Dropdown.OptionData();
-            option.text = item.width + "x" + item.height + " " + item.refreshRate + "hz";
-            resolutionDropdown.options.Add(option);
-
-            if (item.width == Screen.width && item.height == Screen.height)
-                resolutionDropdown.value = optionNum;
-            optionNum++;
-        }
-        resolutionDropdown.RefreshShownValue();
-
-        windowedscreenBtn.isOn = Screen.fullScreenMode.Equals(FullScreenMode.Windowed) ? true : false;
-    }
-
     public void DropboxOptionChange(int x)
     {
-        resolutionNum = x;
+        switch (dropdown.value)
+        {
+            case 0:
+                Screen.SetResolution(1920, 1080, screenMode);
+                break;
+            case 1:
+                Screen.SetResolution(1600, 900, screenMode);
+                break;
+            case 2:
+                Screen.SetResolution(1280, 720, screenMode);
+                break;
+        }
+        Debug.Log(dropdown.value);
+        windowedscreenBtn.isOn = Screen.fullScreenMode.Equals(FullScreenMode.Windowed) ? true : false;
     }
     public void WindowedScreenBtn(bool isWIndowed)
     {
         screenMode = isWIndowed ? FullScreenMode.Windowed : FullScreenMode.FullScreenWindow;
-    }
-    public void BtnClick()
-    {
-        Screen.SetResolution(resolutions[resolutionNum].width,
-            resolutions[resolutionNum].height,
-            screenMode);
     }
 }
