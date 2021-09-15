1e160228d573fd993bba9fc7e8dd2581cbd8efe9
Horim1 <72972218+horimihorimi@users.noreply.github.com>
2021-09-15 22:32:47 +0900

    branch:origin/HB 병합
    - Conversation 씬 추가
    - HB Script 폴더 생성
     * MSG_Send (어느 버튼 클릭 되었는지 메시지 전달 스크립트)
     * Schedule_Button (심문에서 사용되는 버튼에 관한 스크립트)
     * Text_Clickable (대화를 통해 증거 수집할 수 있도록 도와주는 스크립트)
     * Text_color (Text_Clickable 하고 병합 예정 스크립트)
     * Text_Event (수집한 증거 즉, 클릭한 텍스트를 저장하는 스크립트 Text_Clickable과 병합 예정)
    - TextMeshPro 한글 사용을 위해 Asset/Fonts 경로에 HB_Font 폴더 생성
    - TextMeshpro 오류 해결 위해 TMP Essential Resources를 Import 함

---
+++ Workpeice/programming/Socializer/Assets/Scripts/HB_Script/Schedule_Button.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Schedule_Button : MonoBehaviour
+{
+    public GameObject Button;
+    public int Actiononoff = 0;
+    [SerializeField] Texture2D cursorImg1;
+    [SerializeField] Texture2D cursorImg2;
+    //public Text_Event TE = FindObjectOfType<Text_Event>();
+   // public GameObject TE;
+
+    public void Clicked()
+    {
+        //TE = GameObject.Find("StoryText");       
+        GameObject.Find("SayDialog").transform.Find("Panel2");
+        Button = EventSystem.current.currentSelectedGameObject;
+        if(Button.name == "Observation_btn")
+        {
+            switch (Actiononoff) {
+                case 0:
+                    Debug.Log("관찰 활성화");
+                    Actiononoff = 1;
+                    Cursor.SetCursor(cursorImg1, Vector2.zero, CursorMode.ForceSoftware);
+                    GameObject.Find("StoryText").GetComponent<Text_Clickable>().enabled = true;                 
+                    //TE.GetComponent<Text_Event>().Event();
+                    break;
+
+                case 1:
+                    Debug.Log("관찰 비활성화");
+                    gameObject.GetComponent<Text_Clickable>().enabled = false;
+                    Actiononoff = 0;
+                    Cursor.SetCursor(cursorImg2, Vector2.zero, CursorMode.ForceSoftware);
+                    break;                
+            }
+        }
+        
+        else if(Button.name == "Action_btn")
+        {
+            GameObject.Find("SayDialog").transform.Find("Panel").transform.Find("Button").transform.Find("Pass_btn").gameObject.SetActive(true);
+            GameObject.Find("SayDialog").transform.Find("Panel").transform.Find("Button").transform.Find("Arrest_btn").gameObject.SetActive(true);
+            GameObject.Find("SayDialog").transform.Find("Panel").transform.Find("Button").transform.Find("Kill_btn").gameObject.SetActive(true);
+            Debug.Log("행동 버튼 클릭");
+
+        }
+    }
+}
