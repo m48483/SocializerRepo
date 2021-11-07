c04b4315ec0a7b964b1c5a0fea423d799578a351
m48483 <71882579+m48483@users.noreply.github.com>
2021-11-08 03:06:50 +0900

    프리팹 재설정

--- Workpeice/programming/Socializer/Assets/Scripts/boo/AudioManager.cs
+++ Workpeice/programming/Socializer/Assets/Scripts/boo/AudioManager.cs
@@ -11,16 +11,69 @@ public class AudioManager : MonoBehaviour
     {
         openSliders = GetComponent<OpenSliders>();
 
-        if (instance != null)
+        //if (instance != null)
+        //{
+        //    Destroy(this.gameObject);
+        //    Debug.Log(OpenSliders.isSliderOn);
+        //    OptionPopupSystem.isOn = false;
+        //}
+        //else
+        //{
+        //    instance = this;
+        //    DontDestroyOnLoad(this.gameObject);
+        //}
+
+        //var obj = FindObjectsOfType<AudioManager>();
+
+        //if (obj.Length == 1)
+        //{
+        //    DontDestroyOnLoad(this.gameObject);
+        //}
+        //else
+        //{
+        //    Destroy(gameObject);
+        //}
+        if (SceneManager.GetActiveScene().buildIndex == 0
+            || SceneManager.GetActiveScene().buildIndex == 2)
         {
-            Destroy(this.gameObject);
-            Debug.Log(OpenSliders.isSliderOn);
-            OptionPopupSystem.isOn = false;
+            //SceneManager.GetActiveScene().name == "Title"
+            if (instance != null)
+            {
+                Destroy(this.gameObject);
+                Debug.Log(OpenSliders.isSliderOn);
+                OptionPopupSystem.isOn = false;
+            }
+            else
+            {
+                instance = this;
+                DontDestroyOnLoad(this);
+                Debug.Log(SceneManager.GetActiveScene().buildIndex);
+            }
         }
         else
         {
-            instance = this;
-            DontDestroyOnLoad(this);
+            GameObject.Destroy(this);
+            //Destroy(this.gameObject);
+        }
+
+        //if (instance != null)
+        //{
+        //    Destroy(this.gameObject);
+        //    Debug.Log(OpenSliders.isSliderOn);
+        //    OptionPopupSystem.isOn = false;
+        //}
+        //else
+        //{
+        //    instance = this;
+        //    DontDestroyOnLoad(this);
+        //}
+    }
+    private void Update()
+    {
+        if (SceneManager.GetActiveScene().buildIndex != 0
+            || SceneManager.GetActiveScene().buildIndex != 2)
+        {
+            GameObject.Destroy(this);
         }
     }
 }
