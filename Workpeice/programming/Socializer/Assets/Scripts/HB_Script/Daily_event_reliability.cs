49525a254de209ed8cf1dd2c047ba09c9a81af60
horimihorimi <72972218+horimihorimi@users.noreply.github.com>
2022-03-26 16:49:28 +0900

    17, 18일 대사 flowchart 추가

---
+++ Workpeice/programming/Socializer/Assets/Scripts/HB_Script/Daily_event_reliability.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Fungus;
+
+
+public class Daily_event_reliability : MonoBehaviour
+{
+    float Hilda_reliability;
+    float Heather_reliability;
+    float Frances_reliability;
+    float Dylan_reliability;
+
+    public void Day18(int choice)
+    {
+        if (choice == 1)
+        {
+            Frances_reliability = GameObject.Find("Variables").GetComponent<Flowchart>().GetFloatVariable("Frances_reliability");
+            Frances_reliability -= 5;
+            GameObject.Find("Variables").GetComponent<Flowchart>().SetFloatVariable("Frances_reliability", Frances_reliability);
+        }
+        else if (choice == 2)
+        {
+            Frances_reliability = GameObject.Find("Variables").GetComponent<Flowchart>().GetFloatVariable("Frances_reliability");
+            Frances_reliability += 5;
+            GameObject.Find("Variables").GetComponent<Flowchart>().SetFloatVariable("Frances_reliability", Frances_reliability);
+        }
+    }
+
+}
