8926f3742f5c6785748175186edaeb8fcd928369
m48483 <71882579+m48483@users.noreply.github.com>
2022-06-24 19:02:34 +0900

    랜덤 npc 스크립트 완성
    - 랜덤 npc 등장 스크립트 완성 (추후 보완 필요성 있을 수 있음)

---
+++ Workpeice/programming/Socializer/Assets/Scripts/boo/LoadSceneManager.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadSceneManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        LoadSceneAdditive();
+    }
+
+    private void LoadSceneAdditive()
+    {
+        SceneManager.LoadScene("Variables", LoadSceneMode.Additive);
+    }
+}
