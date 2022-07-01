a6b9445447f56771457cf59df01560fa7dcb7b4d
m48483 <71882579+m48483@users.noreply.github.com>
2022-07-02 00:57:50 +0900

    2022.07.02.
    - 5일차 보고서 띄우고 5일차에 호감도 엔딩으로 넘어가도록 스크립트 작성(1차 프로토타입용)
    - 보고서 Scene 끝나고 중앙정보부(힐다, 딜런) 호감도 0이면 게임오버 엔딩으로 이동

--- Workpeice/programming/Socializer/Assets/Scripts/boo/TestEnding.cs
+++ Workpeice/programming/Socializer/Assets/Scripts/boo/TestEnding.cs
@@ -1,16 +1,28 @@
-﻿using System.Collections;
+﻿using Fungus;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class TestEnding : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        
+        //GameOverEnding();
     }
-    void AfterDay5()
+    public void AfterDay5()
     {
-        //if()
+        SceneManager.LoadScene("Ending2");
+    }
+    public void GameOverEnding()
+    {
+        float h = GameObject.Find("Variables").GetComponent<Flowchart>().GetFloatVariable("Hilda_reliability");
+        float d = GameObject.Find("Variables").GetComponent<Flowchart>().GetFloatVariable("Dylan_reliability");
+
+        if(h== 0 && d == 0)
+        {
+            SceneManager.LoadScene("Ending1");
+        }
     }
 }
