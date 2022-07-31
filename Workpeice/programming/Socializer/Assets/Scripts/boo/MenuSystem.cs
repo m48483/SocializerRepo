b2784bb89617949206ace96920d6335bd2e75e3e
m48483 <71882579+m48483@users.noreply.github.com>
2022-08-01 00:36:12 +0900

    2022.08.01.
    - 팝업 창 새롭게 제작

---
+++ Workpeice/programming/Socializer/Assets/Scripts/boo/MenuSystem.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuSystem : MonoBehaviour
+{
+    public GameObject popup;
+    public GameObject btn_popup;
+    public GameObject panel;
+    // Start is called before the first frame update
+
+    public void OpenPopup()//Action onClickBtn)
+    {
+        //this.onClickBtn = onClickBtn;
+        popup.SetActive(true);
+        panel.SetActive(true);
+        Debug.Log("PopupOn");
+    }
+
+    public void OnClickBtn()
+    {
+        ClosePopUP();
+    }
+
+    void ClosePopUP()
+    {
+        popup.SetActive(false);
+        panel.SetActive(false);
+        Debug.Log("PopupOff");
+    }
+}
